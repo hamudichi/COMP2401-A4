817ebe9da91727c4dfe818c04412e3a6f1be858b
Mohamad Yassine <hamudichi@gmail.com>
2016-04-01 23:42:06 -0400

    updated client header - added ANSI_COLOR_GREY

--- client.h
+++ client.h
@@ -1,4 +1,3 @@
-
 /* Include Gaurds - used for making sure the file is only read once or included once */
 #ifndef client_h
 #define client_h
@@ -21,6 +20,7 @@ typedef enum err {
 #define ANSI_COLOR_BLUE  "\x1b[34m"
 #define ANSI_COLOR_CYAN "\x1b[36m"
 #define ANSI_COLOR_RESET "\x1b[0m"
+#define ANSI_COLOR_GREY  "\x1b[40m"
 #define ANSI_GREY_BCK "\x1b[01;40m"
 #define ANSI_ITALIC "\x1b[03m"
 #define ANSI_UNDER "\x1b[04m"
